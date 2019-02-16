c28dd1cd4feddbc490aebfc0f8aeae535f1b9ce1
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2019-02-16 17:43:09 +0100

    Implemened bouncing from slider

--- Ball.cpp
+++ Ball.cpp
@@ -1,7 +1,7 @@
 #include <odroid_go.h>
 #include "Ball.h"
 
-const int DEFAULT_RADIUS = 5;
+const int DEFAULT_RADIUS = 3;
 const float DEFAULT_SPEED = 4;
   
 Ball::Ball(Slider& slider) {
@@ -50,24 +50,32 @@ void Ball::updatePosition(Level& level, Slider& slider, unsigned int screenWidth
   positionX += speedX;
   positionY += speedY;
   
-  if (positionX + radius > (screenWidth - level.getBorderRight())) {                    // bounce off right
+  if (positionX + radius >= (screenWidth - level.getBorderRight())) {                    // bounce off right
     positionX -= 2 *((positionX + radius) - (screenWidth - level.getBorderRight()));
     speedX *= -1;
-  } else if (positionX - radius < level.getBorderLeft()) {                              // bounce off left
+  } else if (positionX - radius <= level.getBorderLeft()) {                              // bounce off left
     positionX -= 2 *((positionX - radius) - (level.getBorderLeft()));
     speedX *= -1;
   }
 
-  if (positionY - radius < level.getBorderTop()) {
+  if (positionY - radius <= level.getBorderTop()) {                                      // bounce off top
     positionY += 2 *((positionY - radius) + (level.getBorderTop()));
     speedY *= -1;
-  } else {
-    
+  } else if ((speedY > 0) && (positionY + radius >= slider.getPositionY()) && 
+             (positionY - speedY + radius < slider.getPositionY()) && 
+             (positionX >= slider.getPositionX()) && 
+             (positionX <= slider.getPositionX() + slider.getWidth())) {
+    positionY -= 2 *((positionY + radius) - slider.getPositionY());
+    speedY *= -1;
+  }
+
+  if (positionY > screenHeight) {
+    // Game over
   }
   
 }
 
 void Ball::updatePositionOnSlider(Slider& slider) {
   positionX = slider.getPositionX() + slider.getWidth() / 2;
-  positionY = slider.getPositionY() -3;
+  positionY = slider.getPositionY() - radius;
 }
